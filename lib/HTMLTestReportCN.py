708afda9409137cda6c1ba6ddd9a3c6d44b00042
ruanxiujuan <2969775177@163.com>
2019-08-15 16:01:40 +0800

    HTMLTestReportCN 原文件中的概要为通过率

--- lib/HTMLTestReportCN.py
+++ lib/HTMLTestReportCN.py
@@ -629,7 +629,7 @@ table       { font-size: 100%; }
     REPORT_TMPL = """
 <div style="width: 500px; clear: both;">
 <p id='show_detail_line'>
-<a class="btn btn-primary" href='javascript:showCase(0)'>概要{ %(passrate)s }</a>
+<a class="btn btn-primary" href='javascript:showCase(0)'>通过率{ %(passrate)s }</a>
 <a class="btn btn-success" href='javascript:showCase(2)'>通过{ %(Pass)s }</a>
 <a class="btn btn-danger" href='javascript:showCase(1)'>失败{ %(fail)s }</a>
 <a class="btn btn-warning" href='javascript:showCase(3)'>错误{ %(error)s }</a>
